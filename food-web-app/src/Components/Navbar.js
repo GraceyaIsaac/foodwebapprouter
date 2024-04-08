4d21790f3eb6f1ba8345e8d813f9be69e13fd8e5
GraceyaIsaac <graceya1105@gmail.com>
2024-04-08 16:41:21 +0530

    fix: added useState to handle the screen changes

--- food-web-app/src/Components/Navbar.js
+++ food-web-app/src/Components/Navbar.js
@@ -1,19 +1,35 @@
-import React from "react"; 
+import React,{useState} from "react"; 
 import '../styles/Navbar.css'
 import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder'
 import Logo from '../assets/pizzaLogo.png';
 
 const Navbar = () => {
+    const[openLinks,setOpenLinks]=useState(false)
+//function
+  const toggleNavbar=()=>{
+    setOpenLinks(!openLinks)
+  }
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ? "open":"close"}>
         <img src={Logo} alt="Pizza Logo" />
+        <div className="hiddenLinks">
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link> 
+        </div>
       </div>
       <div className="rightSide">
             <Link to="/">Home</Link>
             <Link to="/menu">Menu</Link>
             <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>      
+            <Link to="/contact">Contact</Link> 
+            <button onClick={toggleNavbar}>
+                <ReorderIcon/>
+            </button>
+             
       </div>
       
     </div>
